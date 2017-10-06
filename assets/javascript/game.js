aa25b50c9c5be9056a5e25afa495adbcfa0470e1
raphatapi <raphaeltapioca10@gmail.com>
2017-10-06 00:59:37 -0600

    progress!

---
+++ assets/javascript/game.js
@@ -0,0 +1,62 @@
+//Words in the game
+var words = ["strange", "eerie", "wraith", "specter", "haunt", "uncanny", "eldritch", "weird", "bizarre", "dreadful", "terror"];
+var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var userGuesses = [];
+var count = 10;
+
+$(document).ready(function() {
+    $("#theme").get(0).play();
+});
+//Make pause button work
+
+function createLetterButtons() {
+
+      for (var i=0; i<letters.length; i++) {
+        var letterBtn = $("<button>");
+        letterBtn.addClass("letter-button letter letter-button-color");
+        letterBtn.attr("data-letter", letters[i]);
+        letterBtn.text(letters[i]);
+        $("#buttons").append(letterBtn);
+
+      }
+
+      $(".letter-button").on("click", function(){
+      	if($("*#hidden-letter-" + $(this).attr("data-letter")).length !== 0){
+      		console.log(this);
+      		console.log("found 1");
+      		$("*#hidden-letter-" + $(this).attr("data-letter")).text($(this).attr("data-letter"));
+      		$(this).remove();
+      		//End the game if word is correct!
+      	}else{
+      		count -= 1;
+	      	$("#wrong-letters").append(this); //Need to fix this shit!
+	      	console.log(count);
+	        if(count==0){
+	        	$("#buttons").remove();
+	        	$("#title").text("Life is too short");
+	        	$("#title").append("<br><button onclick='reloadPage()'>Play Again!</button>");
+	        }
+	      	$(this).remove();
+      	}
+      	
+      	
+	})
+};
+
+function chooseWord () {
+    var word = words[Math.floor(Math.random() * words.length)];
+    hiddenWord(word.toUpperCase());
+}
+
+function hiddenWord (word) {  
+	$.each(word.split(""), function (i, el) {
+        $("#blank-word").append("<span id='hidden-letter-" + el + "'>_ </span");
+    });
+	
+ }
+
+ function reloadPage() {
+    location.reload();
+}
+
+
